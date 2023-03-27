6026483ce3c1eda04f47e35e339bdd7c518c92e0
Onozuka Shunsuke <shunsuke.onozuka@gmail.com>
2023-03-27 20:51:06 +0900

    readline.tsの修正

--- practice/src/readline.ts
+++ practice/src/readline.ts
@@ -5,7 +5,8 @@ const rl = createInterface({
   output: process.stdout,
 });
 
-rl.question("文字列を入力してください：", (line) => {
-  console.log(`${line}が入力されました`);
+rl.question("数値を入力してください：", (line) => {
+  const result = line + 1000;
+  console.log(result);
   rl.close();
 });
