1351e65db1729be978925426386283ce384e1648
Onozuka Shunsuke <shunsuke.onozuka@gmail.com>
2023-04-02 22:38:42 +0900

    2.4.6を実施

---
+++ practice/src/2章_基本的な文法・基本的な型/2.4_演算子/2.4.6_論理演算子（2）一般形と短絡評価/1.ts
@@ -0,0 +1,13 @@
+/* !!式の実用例 */
+{
+  const input1 = "123",
+    input2 = "";
+
+  const input1isNotEmpty = !!input1;
+  console.log(input1isNotEmpty);
+  // >true
+
+  const input2isNotEmpty = !!input2;
+  console.log(input2isNotEmpty);
+  // >false
+}
