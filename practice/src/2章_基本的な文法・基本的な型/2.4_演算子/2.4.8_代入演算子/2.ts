221839648bd7fb5fe5908b13d312f265d611d561
Onozuka Shunsuke <shunsuke.onozuka@gmail.com>
2023-04-02 23:17:22 +0900

    2.4.8を実施

---
+++ practice/src/2章_基本的な文法・基本的な型/2.4_演算子/2.4.8_代入演算子/2.ts
@@ -0,0 +1,5 @@
+{
+  let foo: string = "文字列";
+  // foo = 123;
+  // >型 'number' を型 'string' に割り当てることはできません。
+}
---
+++ practice/src/2章_基本的な文法・基本的な型/2.4_演算子/2.4.8_代入演算子/3.ts
@@ -0,0 +1,18 @@
+{
+  let num = 0;
+
+  console.log((num += 100));
+  // >100
+
+  console.log((num *= 4));
+  // >400
+
+  console.log((num -= 200));
+  // >200
+
+  console.log((num /= 2));
+  // >100
+
+  console.log((num **= 0.5));
+  // >10
+}
