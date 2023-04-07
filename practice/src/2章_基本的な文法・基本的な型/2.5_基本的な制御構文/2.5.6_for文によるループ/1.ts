c8b2bed3f02327851335de0d6ca3e6d3703df92b
Onozuka Shunsuke <shunsuke.onozuka@gmail.com>
2023-04-07 21:40:15 +0900

    2.5.6の実施

---
+++ practice/src/2章_基本的な文法・基本的な型/2.5_基本的な制御構文/2.5.6_for文によるループ/1.ts
@@ -0,0 +1,8 @@
+{
+  let sum = 0;
+  for (let i: number = 1; i <= 100; i++) {
+    sum += i;
+  }
+  console.log(sum);
+  // >5050
+}
---
+++ practice/src/2章_基本的な文法・基本的な型/2.5_基本的な制御構文/2.5.6_for文によるループ/2.ts
@@ -0,0 +1,8 @@
+{
+  for (let i = 1; i <= 100; i++) {
+    if (i % 2 === 1) {
+      continue;
+    }
+    console.log(i);
+  }
+}
