203715c6a67b7eb03b9fee04b10cf240266be397
Onozuka Shunsuke <shunsuke.onozuka@gmail.com>
2023-04-22 21:57:18 +0900

    3.8実施

--- practice/src/3章_オブジェクトの基本とオブジェクトの型/3.8_力試し/3.8.2_解説/1.ts
+++ practice/src/3章_オブジェクトの基本とオブジェクトの型/3.8_力試し/3.8.2_解説/1.ts
@@ -11,8 +11,23 @@ John Smith,17,0
 Mary Sue,14,1
 `;
 
-  // ここにコードを足す
   const users: User[] = [];
+
+  const lines = data.split("\n");
+  console.log(lines);
+
+  for (const line of lines) {
+    if (line === "") {
+      continue;
+    }
+    const [name, ageString, premiumUserString] = line.split(",");
+    const age = Number(ageString);
+    const premiumUser = premiumUserString === "1";
+
+    users.push({ name, age, premiumUser });
+  }
+
+  console.log(users);
 
   for (const user of users) {
     if (user.premiumUser) {
--- practice/src/3章_オブジェクトの基本とオブジェクトの型/3.8_力試し/3.8.3_別解/1.ts
+++ practice/src/3章_オブジェクトの基本とオブジェクトの型/3.8_力試し/3.8.3_別解/1.ts
@@ -11,8 +11,18 @@ John Smith,17,0
 Mary Sue,14,1
 `;
 
-  // ここにコードを足す
-  const users: User[] = [];
+  const users: User[] = data
+    .split("\n")
+    .filter((line) => line !== "")
+    .map((line) => {
+      const [name, ageString, premiumUserString] = line.split(",");
+
+      return {
+        name,
+        age: Number(ageString),
+        premiumUser: premiumUserString === "1",
+      };
+    });
 
   for (const user of users) {
     if (user.premiumUser) {
