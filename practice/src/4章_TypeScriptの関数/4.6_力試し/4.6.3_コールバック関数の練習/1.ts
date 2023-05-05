9083c1d1ef3dffda394732cbce7fdd2de4b5f210
Onozuka Shunsuke <shunsuke.onozuka@gmail.com>
2023-05-05 23:51:31 +0900

    ジェネリクス対応

--- practice/src/4章_TypeScriptの関数/4.6_力試し/4.6.3_コールバック関数の練習/1.ts
+++ practice/src/4章_TypeScriptの関数/4.6_力試し/4.6.3_コールバック関数の練習/1.ts
@@ -1,14 +1,14 @@
 {
-  function map(array: number[], callback: (value: number) => number): number[] {
-    const result: number[] = [];
+  function map<T, K>(array: T[], callback: (value: T) => K): K[] {
+    const result: K[] = [];
     for (const elm of array) {
       result.push(callback(elm));
     }
     return result;
   }
 
-  const data = [1, 1, 2, 3, 5, 8, 13];
+  const data = [1, -3, -2, 8, 0, -1];
 
-  const result = map(data, (x) => x * 10);
+  const result: boolean[] = map(data, (x) => x >= 0);
   console.log(result);
 }
